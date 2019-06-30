fdea3dacdf93b1eb4ebeeb478f1cfe6bd56a7491
Jess Pumphrey <jmp@jmpumphrey.com>
2019-06-30 12:53:58 +0100

    Initial tweaks
    This is a copy of the version from the ISO, along with some tweaks to make it work on my machine, and logging.

--- samplerbox.py
+++ samplerbox.py
@@ -15,10 +15,10 @@
 #########################################
 
 AUDIO_DEVICE_ID = 2                     # change this number to use another soundcard
-SAMPLES_DIR = "."                       # The root directory containing the sample-sets. Example: "/media/" to look for samples on a USB stick / SD card
-USE_SERIALPORT_MIDI = False             # Set to True to enable MIDI IN via SerialPort (e.g. RaspberryPi's GPIO UART pins)
-USE_I2C_7SEGMENTDISPLAY = False         # Set to True to use a 7-segment display via I2C
-USE_BUTTONS = False                     # Set to True to use momentary buttons (connected to RaspberryPi's GPIO pins) to change preset
+SAMPLES_DIR = "/media/"                       # The root directory containing the sample-sets. Example: "/media/" to look for samples on a USB stick / SD card
+USE_SERIALPORT_MIDI = True             # Set to True to enable MIDI IN via SerialPort (e.g. RaspberryPi's GPIO UART pins)
+USE_I2C_7SEGMENTDISPLAY = True         # Set to True to use a 7-segment display via I2C
+USE_BUTTONS = True                     # Set to True to use momentary buttons (connected to RaspberryPi's GPIO pins) to change preset
 MAX_POLYPHONY = 80                      # This can be set higher, but 80 is a safe value
 
 
@@ -190,11 +190,14 @@ def AudioCallback(outdata, frame_count, time_info, status):
 def MidiCallback(message, time_stamp):
     global playingnotes, sustain, sustainplayingnotes
     global preset
+
     messagetype = message[0] >> 4
     messagechannel = (message[0] & 15) + 1
     note = message[1] if len(message) > 1 else None
     midinote = note
     velocity = message[2] if len(message) > 2 else None
+
+    print("type", messagetype, "channel", messagechannel, "note", note, "velocity", velocity)
 
     if messagetype == 9 and velocity == 0:
         messagetype = 8
@@ -219,6 +222,16 @@ def MidiCallback(message, time_stamp):
     elif messagetype == 12:  # Program change
         print 'Program change ' + str(note)
         preset = note
+        LoadSamples()
+
+    elif messagetype == 11 and (note == 113) and (velocity > 64):
+        preset = preset + 1
+        print "new preset " + str(preset)
+        LoadSamples()
+
+    elif messagetype == 11 and (note == 112) and (velocity > 64):
+        preset = preset - 1
+        print "new preset " + str(preset)
         LoadSamples()
 
     elif (messagetype == 11) and (note == 64) and (velocity < 64):  # sustain pedal off
